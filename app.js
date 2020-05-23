c19a573bdf129f0d7f4930fc88ec07c5e12d180c
weesje <klara.kutas@yahoo.com>
2020-05-23 02:47:00 +0200

    Add extra console log

--- app.js
+++ app.js
@@ -19,3 +19,4 @@ app.use('/cars', carRoutes);
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'));
 console.log('Express server listening on port ' + app.get('port'));
+console.log('hello');
